a57f7460d6cd7e23c326c1995e2432b075c56fca
istkharroohi78 <istkharroohi@gmail.com>
2026-07-08 20:27:11 +0530

    Update storage.py

--- ISTKHAR_CHATBOT/modules/helpers/storage.py
+++ ISTKHAR_CHATBOT/modules/helpers/storage.py
@@ -1,4 +1,3 @@
-import random
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 CHAT_STORAGE = [
@@ -14,7 +13,21 @@ CHAT_STORAGE = [
     "mongodb+srv://chatbot10:j@cluster0.9esnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 ]
 
-ISTKHARBOY = MongoCli(random.choice(CHAT_STORAGE))
+# Saare 10 clusters ko ek saath connect karke list bana lo
+db_clients = [MongoCli(url) for url in CHAT_STORAGE]
+
+def get_user_db_client(user_id: int):
+    """
+    Yeh function Random ki jagah User ID calculate karke hamesha
+    us user ke liye wahi same database return karega.
+    """
+    # 10 DBs me load balance karna
+    db_index = user_id % len(db_clients)
+    return db_clients[db_index]
+
+# Aapke purane variables (Agar existing bot logic me directly inko use kar rahe the)
+# Note: Agar global variables chahiye hi chahiye, toh by default Cluster 1 assign kar diya hai
+ISTKHARBOY = db_clients[0] 
 chatdb = ISTKHARBOY.Anonymous
 chatai = chatdb.Word.WordDb
-storeai = ISTKHARBOY.Anonymous.Word.NewWordDb  
+storeai = ISTKHARBOY.Anonymous.Word.NewWordDb
